4210167563cb49e76a47bd7d48d6928a53a21304
binhkbb2004-hash <binhkbb2004@gmail.com>
2026-06-09 22:49:19 +0700

    Update

--- rtl/extract_bit.v
+++ rtl/extract_bit.v
@@ -8,20 +8,20 @@ module extract_bit (
     output reg o_sof // Báo hiệu cặp bit đầu tiên của chuỗi      
 );
     reg [15:0] shift_reg;
-    reg [3:0] count; // Tự đếm số lần dịch
+    reg [2:0] count; // Tự đếm số lần dịch
 
     assign o_rx = o_valid ? shift_reg[15:14] : 2'b00; // Dùng 1 thanh ghi dịch để xuất các cặp bit
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             shift_reg <= 16'd0;
-            count     <= 4'd0;
+            count     <= 3'd0;
             o_valid   <= 1'b0;
             o_sof     <= 1'b0;
         end else if (i_start) begin
             // Nhận chuỗi mới
             shift_reg <= i_data;
-            count     <= 4'd7; // Còn 7 lần dịch nữa
+            count     <= 3'd7; // Còn 7 lần dịch nữa
             o_valid   <= 1'b1;
             o_sof     <= 1'b1; // Cờ báo cặp bit đầu lên 1 nhịp
         end else if (count > 0) begin
